6060c8e2096f84576c1bbfe826544ccb6bd7ebca
trovtle basnet <basnet.trovtle@gmail.com>
2021-05-18 16:17:32 +1000

    Minor fixes

--- pages/index.js
+++ pages/index.js
@@ -114,7 +114,12 @@ export default function Home({ movieData }) {
 					href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;1,200;1,300;1,400;1,600;1,700&display=swap"
 					rel="stylesheet"
 				/>
-				<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+				<meta name="description" content="Star Wars Fan Website" />
+				<meta
+					http-equiv="Content-Security-Policy"
+					content="upgrade-insecure-requests"
+				/>
+				<title>Swapi Movie</title>
 			</Head>
 			<div className={styles.HomeScreen}>
 				<div className={styles.HomeScreen__container}>
